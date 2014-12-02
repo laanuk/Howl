614999941ad3b22c78f983ec9a658dcc9ba526d6
Kunaal Naik <laanuk13@gmail.com>
2014-12-01 23:49:03 -0500

    final update

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
-var wolfram = require('wolfram').createClient("E644RW-T448XKJPY6");
 var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/query', function(req, res) {
-	console.log(req);
-	wolfram.query(req.query.input, function(err, result) {
-  		if(err) throw err
-  		console.log("Result: %j", result);
-		res.send(result);
-	});
-});
+
 
 app.listen(process.env.PORT || 8000);
